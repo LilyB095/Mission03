cb28d601f771ae60f8f1e5403c54027396a6aae0
LilyB095 <lily.barton095@gmail.com>
2022-01-19 15:20:09 -0700

    Add project files.

---
+++ Mission03/Models/CalculatorModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission03.Models
+{
+    public class CalculatorModel
+    {
+        [Required(ErrorMessage = "An Album Title is required")]
+        //[Range(0, 100)]
+        public int Assignment { get; set; }
+
+        [Required(ErrorMessage ="Hello")]
+        [Range(0, 100)]
+        public int Group { get; set; }
+
+        [Required]
+        [Range(0, 100)]
+        public int Quiz { get; set; }
+
+        [Required]
+        [Range(0, 100)]
+        public int Exam { get; set; }
+
+        [Range(0, 100)]
+        public int Intex { get; set; }
+    }
+
+}
---
+++ Mission03/wwwroot/js/mission2.js
@@ -0,0 +1,58 @@
+﻿// Everytime the Calculate button is clicked, 
+// determine and print out grade to the right spot on index.html
+
+$("#calc").click(function () {
+
+    // Find number grade by multiplying each category by the weight
+        //55 % - Assignments
+        //5%   - Group Project
+        //10 % - Quizzes
+        //20 % - Exams
+        //10 % - INTEX
+    var numgrade = $("#assignment").val() * .55 + $("#group").val() * .05 +
+                    $("#quiz").val() * .1 + $("#exam").val() * .2 + $("#intex").val() * .1
+
+    // Determine letter grade based on number grade
+    if (numgrade >= 94) {
+        var lettergrade = "A"
+    }
+    else if (numgrade >= 90) {
+        var lettergrade = "A-"
+    }
+    else if (numgrade >= 87) {
+        var lettergrade = "B+"
+    }
+    else if (numgrade >= 84) {
+        var lettergrade = "B"
+    }
+    else if (numgrade >= 80) {
+        var lettergrade = "B-"
+    }
+    else if (numgrade >= 77) {
+        var lettergrade = "C+"
+    }
+    else if (numgrade >= 74) {
+        var lettergrade = "C"
+    }
+    else if (numgrade >= 70) {
+        var lettergrade = "C-"
+    }
+    else if (numgrade >= 67) {
+        var lettergrade = "D+"
+    }
+    else if (numgrade >= 64) {
+        var lettergrade = "D"
+    }
+    else if (numgrade >= 60) {
+        var lettergrade = "D-"
+    }
+    else {
+        var lettergrade = "E"
+    }
+
+    // Put the lettergrade and numeric grade together into the html header with the id "grade"
+    $("#grade").html("Your grade is: " + lettergrade + " (" + numgrade + "%)");
+    // alert with lettergrade and numeric grade
+    alert("You would get: " + lettergrade + " (" + numgrade + "%)");
+
+})
